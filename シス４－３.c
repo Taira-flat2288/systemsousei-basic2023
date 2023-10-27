96c19c40da62ddcbcf323c1b897c19db5224da07
Taira-flat2288 <148538257+Taira-flat2288@users.noreply.github.com>
2023-10-27 19:36:32 +0900

    Add files via upload

---
+++ シス４－３.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+	int n;
+
+	printf("‚T{‚S‚ÍH");
+	scanf_s("%d", &n);
+
+	if (n == 9)
+	{
+		printf("‚Å‚«‚Ä“–‘R");
+	}
+	else
+	{
+		printf("‚ ‚È‚½‚Í‰‚Å‚·‚©H");
+	}
+	return 0;
+}
